74ea0aff56e6e6a1826790c16e65aa34354bf95e
Yohei Sugigami <y.sugigami@gmail.com>
2016-02-12 17:18:33 +0900

    Add target for OSX framework

---
+++ OctoKitOSX/OctoKitOSX.h
@@ -0,0 +1,19 @@
+//
+//  OctoKitOSX.h
+//  OctoKitOSX
+//
+//  Created by Piet Brauer on 25/08/15.
+//  Copyright (c) 2015 nerdish by nature. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OctoKitOSX.
+FOUNDATION_EXPORT double OctoKitOSXVersionNumber;
+
+//! Project version string for OctoKitOSX.
+FOUNDATION_EXPORT const unsigned char OctoKitOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OctoKitOSX/PublicHeader.h>
+
+
